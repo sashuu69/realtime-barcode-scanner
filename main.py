9c5a6fee7f35a7c63f769df58375df8beb0afbc6
Sashwat K <sashwat0001@gmail.com>
2019-07-31 23:03:00 +0530

    removed unnecessary imports

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # import the necessary packages
 from imutils.video import VideoStream
 from pyzbar import pyzbar
-import datetime
 import imutils
-import time
 import cv2
 
 # initialize the video stream and allow the camera sensor to warm up
